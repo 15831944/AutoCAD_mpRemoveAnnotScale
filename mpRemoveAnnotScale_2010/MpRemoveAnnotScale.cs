9fddbc6f3e9d23794c4b9309745245738f992df9
cjmodis <cjmodis@gmail.com>
2017-09-03 15:33:57 +0300

    Добавьте файлы проекта.

---
+++ mpRemoveAnnotScale_2010/MpRemoveAnnotScale.cs
@@ -0,0 +1,84 @@
+﻿#if ac2010
+using AcApp = Autodesk.AutoCAD.ApplicationServices.Application;
+#elif ac2013
+using AcApp = Autodesk.AutoCAD.ApplicationServices.Core.Application;
+#endif
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Runtime;
+using mpMsg;
+
+
+namespace mpRemoveAnnotScale
+{
+    public class MpRemoveAnnotScale
+    {
+        [CommandMethod("ModPlus", "mpRemoveAnnotScale", CommandFlags.UsePickSet)]
+        public static void Main()
+        {
+            var doc = AcApp.DocumentManager.MdiActiveDocument;
+            var db = doc.Database;
+            var ed = doc.Editor;
+
+            try
+            {
+                using (var tr = db.TransactionManager.StartTransaction())
+                {
+                    var pso = new PromptSelectionOptions
+                    {
+                        MessageForAdding = "\nВыберите объекты:"
+                    };
+                    var psr = ed.GetSelection(pso);
+                    if (psr.Status != PromptStatus.OK) return;
+
+                    var cm = db.ObjectContextManager;
+                    var occ = cm.GetContextCollection("ACDB_ANNOTATIONSCALES");
+                    var currentscale = occ.CurrentContext as AnnotationScale;
+
+                    foreach (var objId in psr.Value.GetObjectIds())
+                    {
+                        using (var ent = tr.GetObject(objId, OpenMode.ForWrite, false, false))
+                        {
+                            if (ent is MLeader)
+                            {
+                                var ml = ent as MLeader;
+                                if (ml.Annotative == AnnotativeStates.True)
+                                {
+                                    AddScale(ml, currentscale, occ);
+                                    if (ml.ContentType == ContentType.MTextContent)
+                                    {
+                                        MText mt = ml.MText;
+                                        if (mt.Annotative == AnnotativeStates.True)
+                                        {
+                                            AddScale(mt, currentscale, occ);
+                                            ml.MText = mt;
+                                        }
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                if (ent.Annotative == AnnotativeStates.True)
+                                    AddScale(ent, currentscale, occ);
+                            }
+                        }
+                    }
+                    tr.Commit();
+                }
+            }
+            catch (Exception ex)
+            {
+                MpExWin.Show(ex);
+            }
+        }
+        private static void AddScale(DBObject ent, ObjectContext curScale, ObjectContextCollection occ)
+        {
+            ent.AddContext(curScale);
+            foreach (var scale in occ)
+            {
+                if (scale.Name != curScale.Name && ent.HasContext(scale))
+                    ent.RemoveContext(scale);
+            }
+        }
+    }
+}
---
+++ mpRemoveAnnotScale_2015/Interface.cs
@@ -0,0 +1,18 @@
+﻿using mpPInterface;
+
+namespace mpRemoveAnnotScale
+{
+    public class Interface : IPluginInterface
+    {
+        public string Name => "mpRemoveAnnotScale";
+        public string AvailCad => "2015";
+        public string LName => "Очистить масштабы";
+        public string Description => "Очищает список аннотативных масштабов в выбранных аннотативных объектах, оставляя текущий масштаб";
+        public string Author => "Пекшев Александр aka Modis";
+        public string Price => "0";
+    }
+    public class VersionData
+    {
+        public const string FuncVersion = "2015";
+    }
+}
